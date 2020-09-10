295a1b9048c976a61d175efa38d4b63776773933
AKen00 <ajken04@gmail.com>
2020-09-10 15:32:02 -0400

    added language parameters

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -2,6 +2,8 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonEl = document.querySelector("#language-buttons");
+
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -63,6 +65,19 @@ for (var i = 0; i < repos.length; i++) {
     console.log(searchTerm);
   };
 
+  var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+              });
+        } else {
+          alert("Error: " + response.statusText);
+        }
+      });
+  };
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -84,4 +99,19 @@ var getUserRepos = function(user) {
             alert("Unable to connect to GitHub");
   });
 };
+
+    var buttonClickHandler = function(event) {
+        var language = event.target.getAttribute("data-language")
+        if (language) {
+            getFeaturedRepos(language);
+          
+            // clear old content
+            repoContainerEl.textContent = "";
+          }
+    }
+
+    buttonClickHandler()
+
   userFormEl.addEventListener("submit", formSubmitHandler);
+
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
